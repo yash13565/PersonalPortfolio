983ca6c56d9c632aadca89eae0f8daf9cf0e7b6d
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-12-04 12:40:07 +0530

    intial commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I specialize in HTML, CSS, and JavaScript, and I have experience working with React frameworks. I have a Bachelor’s degree in Computer Application from ABVV University. I am passionate about creating intuitive and responsive web designs that enhance the user experience. In my free time, I love playing video games.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
